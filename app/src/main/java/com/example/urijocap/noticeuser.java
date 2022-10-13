20bd3ec3111799056e680a4d7750383f5198957c
Hongse0 <hongseyoung981007@gmail.com>
2022-10-13 15:18:56 +0900

    Initial commit

---
+++ app/src/main/java/com/example/urijocap/noticeuser.java
@@ -0,0 +1,32 @@
+package com.example.urijocap;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+public class noticeuser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.notice_user);
+
+        Toolbar toolbar = findViewById(R.id.next_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("알림 송신");
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home: {
+                finish();
+                return true;
+            }
+        }
+            return super.onOptionsItemSelected(item);
+        }
+
+}
